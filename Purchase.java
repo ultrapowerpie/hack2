12715107c50c522aabc978b0c49b441417d23e44
AlbertH-Ubuntu <albertho2006@gmail.com>
2016-04-02 00:45:47 -0400

    Finished all getPurchase() functions, but need to test them all

--- Purchase.java
+++ Purchase.java
@@ -4,33 +4,60 @@
 import java.net.*;
 import java.io.*;
 
+// Must throw exception for all Net functions because of exceptions caused by URL()
+// Example for http request: http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+// Need (?) to set property for "Accept: application/json"
+// Assuming all http requests are of format: curl -X GET --header "Accept: application/json" <URL_String>
+// Need to do something about Key (parameterize?)
+
 public class Purchase {
 	
+	// Key used for querying Capitol One Nessie data base
 	private static String key = "key=26919f14786c1d9c548f0e653e67c78d";
-	private static URL url;
 	
-	// Parameter: ID of account that made purchases for user
-	// Returns: URL for request to get all purchases by user
-	public static URL makeURL(String account_id) throws Exception {
-		String prefix = "http://api.reimaginebanking.com/accounts/";
-		String suffix = "/purchases?";
-		String spec = prefix + account_id + suffix + key;
-		url = new URL(spec);
+	// Parameters: String for Account's ID
+	// Returns: URL to request all purchases made from the Account
+	private static URL makeURL_Purchase_Account(String account_id) throws Exception {
+		String prefix = "http://api.reimaginebanking.com";
+		String account = "/accounts/" + account_id;
+		String suffix = "/purchases?" + key;
+		String spec = prefix + account + suffix;
+		URL url = new URL(spec);
 		return url;
 	}
 	
-	// Note: must throw exception for all Net functions because of issues
-	// http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+	// Parameters: String for Merchant's ID, String for Account's ID
+	// Returns: URL to request all purchases made to the Merchant from the Account
+	private static URL makeURL_Purchase_Merchant_Account(String merchant_id, String account_id) throws Exception {
+		String prefix = "http://api.reimaginebanking.com";
+		String merchant = "/merchants/" + merchant_id;
+		String account = "/accounts/" + account_id;
+		String suffix = "/purchases?" + key;
+		String spec = prefix + merchant + account + suffix;
+		URL url = new URL(spec);
+		return url;
+	}
 	
-	// Parameter: ID of account that made purchases for user
-	// Returns: String represenation of json objects for all purchases made by user
-	// CHANGE RETURN VALUE TYPE
-	public static String getHistory(String account_id) throws Exception {
-		URL url = makeURL(account_id);
+	// Parameters: String for Merchant's ID
+	// Returns: URL to request all purchases made to the Merchant
+	private static URL makeURL_Purchase_Merchant(String merchant_id) throws Exception {
+		String prefix = "http://api.reimaginebanking.com";
+		String merchant = "/merchants/" + merchant_id;
+		String suffix = "/purchases?" + key;
+		String spec = prefix + merchant + suffix;
+		URL url = new URL(spec);
+		return url;
+	}
+	
+	// Parameters: URL for request
+	// Returns: String represenation of list of JSONs
+	private static String sendPurchaseRequest(URL url) throws Exception {
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		int responseCode = conn.getResponseCode();
-		if (responseCode != 200) {
-			return "INVALID ID";
+		conn.setRequestProperty("Accept", "application/json");
+		String responseMessage = conn.getResponseMessage();
+		System.out.println(responseMessage);
+		if (!responseMessage.equals("OK")) {
+			return "ERROR";
 		}
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		String bodyLine;
@@ -42,12 +69,33 @@ public class Purchase {
 		return response.toString();
 	}
 	
-	public static String getPurchase(String account_id, String merchant_id) {
+	// Parameters: String for Account's ID
+	// Returns: String representation of list of JSONs for all purchases made from the Account
+	public static String getPurchase_Account(String account_id) throws Exception {
+		URL url = makeURL_Purchase_Account(account_id);
+		return sendPurchaseRequest(url);
+	}
+	
+	// Parameters: String for Merchant's ID, String for Account's ID
+	// Returns: String representation of list of JSONs for all purchases made to the Merchant from the Account 
+	public static String getPurchase_Merchant_Account(String merchant_id, String account_id) throws Exception {
+		URL url = makeURL_Purchase_Merchant_Account(merchant_id, account_id);
+		return sendPurchaseRequest(url);
+	}
+	
+	// Parameters: String for Merchant's ID
+	// Returns: String representation of list of JSONs for all purchases made to the Merchant
+	public static String getPurchase_Merchant(String merchant_id) throws Exception {
+		URL url = makeURL_Purchase_Merchant(merchant_id);
+		return sendPurchaseRequest(url);
 	}
 	
 	public static void main(String[] args) throws Exception {
-		String account_id = "56c66be7a73e492741508217";
-		System.out.println(makeURL(account_id));
-		System.out.println(getHistory(account_id));
+		//String account_id = "asdasdas";
+		String account_id_1 = "56c66be7a73e492741508217";
+		String merchant_id_1 = "56c66be6a73e49274150762e";
+		System.out.println(getPurchase_Account(account_id_1));
+		System.out.println(getPurchase_Merchant_Account(merchant_id_1, account_id_1));
+		System.out.println(getPurchase_Merchant(merchant_id_1));
 	}
 }
